2b2a9c2f320045cbe4cd9d7253e04e497f236ccf
Tribekk <anpilogov.sava@gmail.com>
2024-11-24 16:59:42 +0500

    add style all page

--- resources/views/cart/cart.blade.php
+++ resources/views/cart/cart.blade.php
@@ -1,23 +1,25 @@
 <x-header message="Корзина"/>
-@if(sizeof($cart)>0)
-    <h1 class="text-center h1 mb-3">Корзина</h1>
-    <div class="cart container-lg">
-        @foreach($cart as $item)
-            <div class="cart_item d-flex">
-                <img src="storage{{asset($item->preview_photo)}}" alt="" class="w-1/5">
-                <p class="name_item w-2/5 text-center my-auto">{{$item->name}}</p>
-                <p class="price_item w-1/5 text-center my-auto">{{number_format($item->price, 0, '', ' ')}} рублей</p>
-                <a href="{{ route('cart.remove', ['article' => $item->article]) }}" class="w-1/5 text-center my-auto">X</a>
-            </div>
-            <hr class="my-3">
-        @endforeach
-    </div>
-    <div class="total_price container-lg text-right">
-        <p>Итого:</p>
-        <p>{{number_format($totalPrice, 0, '', ' ')}} рублей</p>
-        <a class="button w-1/5 ms-auto m-0" href="{{route('create.order')}}">Оформить заказ</a>
-    </div>
-@else
-    <h1 class="text-center h1 mb-5">Корзина пуста</h1>
-@endif
+<section class="cart">
+    @if(sizeof($cart)>0)
+        <h1 class="text-center mb-3">Корзина</h1>
+        <div class="cart container-lg">
+            @foreach($cart as $item)
+                <div class="cart_item d-flex">
+                    <img src="storage{{asset($item->preview_photo)}}" alt="" class="w-1/5">
+                    <p class="name_item w-2/5 text-center my-auto">{{$item->name}}</p>
+                    <p class="price_item w-1/5 text-center my-auto">{{number_format($item->price, 0, '', ' ')}} рублей</p>
+                    <a href="{{ route('cart.remove', ['article' => $item->article]) }}" class="w-1/5 text-center my-auto">X</a>
+                </div>
+                <hr class="my-3">
+            @endforeach
+        </div>
+        <div class="total_price container-lg text-right">
+            <p>Итого:</p>
+            <p>{{number_format($totalPrice, 0, '', ' ')}} рублей</p>
+            <a class="button" href="{{route('create.order')}}">Оформить заказ</a>
+        </div>
+    @else
+        <h1 class="text-center h1 mb-5">Корзина пуста</h1>
+    @endif
+</section>
 <x-footer/>
--- resources/views/stone/stone.blade.php
+++ resources/views/stone/stone.blade.php
@@ -1,13 +1,13 @@
 <x-header message="Каталог камней"/>
 <link rel="stylesheet" href="{{asset('src/css/stone.css')}}">
-<main class="content container mx-auto">
+<section class="content container-lg mx-auto">
     <div class="catalog d-flex wrap">
         @foreach($stones as $stone)
             <div class="row-4 catalog_item">
                 <img src="{{asset('storage/' . $stone->preview_photo)}}" alt="{{$stone->name}}">
                 <p class="text-center margin-top-10px">{{$stone->name}}</p>
                 <p class="text-center margin-top-10px">{{number_format($stone->price, 0, '', ' ')}} руб.</p>
-                <div class="invisible_card">
+                <div class="invisible_card black">
                     <div class="catalog_item">
                         <a href="{{route('stones.item', ['article' => $stone['article']])}}">
                             <img src="{{asset('storage/' . $stone->next_photo)}}" alt="{{$stone->name}}">
@@ -30,5 +30,5 @@
             </div>
         @endforeach
     </div>
-</main>
+</section>
 <x-footer/>
